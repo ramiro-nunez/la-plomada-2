7c578dc2911fad59fc8a56e4d654a4d7ce4a6281
laran <quintanarijoan@gmail.com>
2026-05-28 21:06:18 -0300

    feat: register con breeze

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,20 @@
 <?php
-namespace App\Http\Controllers;
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactoController;
-use App\Http\Controllers\RegisterLoginController;
 
-/* Rutas que solo renderizan vistas */
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/contactanos', function () {
-    return view('contactanos');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/quienes-somos', function () {
-    return view('quienes-somos');});
-Route::get('/comercio', function () {
-    return view('comercio');
-});
-Route::get('/terms', function () {
-    return view('terminos');
-});
-Route::get('/productos', function () {
-    return view('productos');
-});
-Route::get('/iniciar-sesion', function () {
-    return view('iniciar-sesion');
-});
-Route::get('/registrarse', function () {
-    return view('registro-usuario');
-});
-/* Rutas que utilizan controlador */
-Route::post('/contactanos', [ContactoController::class, 'procesar']);
-Route::post('/registrarse', [RegisterLoginController::class, 'registrar']);
-Route::post('/iniciar-sesion', [RegisterLoginController::class, 'loguear']);
-Route::get('/salir', [RegisterLoginController::class, 'salir']);
+
+require __DIR__.'/auth.php';
