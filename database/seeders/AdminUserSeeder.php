dac3fad1aed5b173de30a92a6538fc60d7b4655d
Ramiro Núñez <ramirosebastiann@gmail.com>
2026-05-30 22:38:53 -0300

    creando usuario admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = new User();
+        $admin->name = 'Super';
+        $admin->lastname = 'Admin'; 
+        $admin->email = 'laplomada@gmail.com';
+        $admin->password = Hash::make('contrasenia123');
+        $admin->role = 'admin'; 
+        $admin->save();
+    }
+}
