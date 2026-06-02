292afffb01551717ef4a0d97849c65b1d3aeae85
Ramiro Núñez <ramirosebastiann@gmail.com>
2026-06-02 18:11:05 -0300

    revision

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-
+//sarasa sarasa
 class AdminUserSeeder extends Seeder
 {
     /**
